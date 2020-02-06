5d9cc702caa6801d12412705d64e55c892f78d12
Robert Jakob <rj@robertjakob.de>
2020-02-07 00:49:29 +0100

    Initial project structure.

---
+++ src/main/kotlin/de/bitflipper/bfstorage/BfstorageApplication.kt
@@ -0,0 +1,11 @@
+package de.bitflipper.bfstorage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BfstorageApplication
+
+fun main(args: Array<String>) {
+	runApplication<BfstorageApplication>(*args)
+}
---
+++ src/test/kotlin/de/bitflipper/bfstorage/BfstorageApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.bitflipper.bfstorage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BfstorageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
